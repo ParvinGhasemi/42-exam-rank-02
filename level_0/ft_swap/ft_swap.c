93335b8d44e1998b5494d5c70f8246b6dce96c7c
Parvin Mohammadghasemi <pamohamm@f0r1s14.codam.nl>
2026-02-26 23:09:01 +0100

    ft_swap.c and its REAME.md was added

--- level_0/ft_swap/ft_swap.c
+++ level_0/ft_swap/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: pamohamm <pamohamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 23:03:41 by pamohamm          #+#    #+#             */
-/*   Updated: 2026/02/26 23:08:04 by pamohamm         ###   ########.fr       */
+/*   Updated: 2026/02/26 23:08:54 by pamohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,4 @@ void ft_swap(int *a, int *b)
 	temp = *a;
 	*a = *b;
 	*b = temp;
-}
-
-#include <stdio.h>
-
-void	ft_swap(int *a, int *b);
-
-int	main(void)
-{
-	int	x = 10;
-	int	y = 42;
-
-	printf("Before swap: x = %d, y = %d\n", x, y);
-
-	ft_swap(&x, &y);
-
-	printf("After swap:  x = %d, y = %d\n", x, y);
-
-	return (0);
 }
