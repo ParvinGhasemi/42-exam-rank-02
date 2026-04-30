8099fcc0d1e68bc133880ee2586d9345b51b5d66
parvinghasemi <mghasemi.parvin@gmail.com>
2026-04-30 22:25:28 +0200

    fixed a bug in printing the numbers

--- level_1/swap_bits/swap_bits.c
+++ level_1/swap_bits/swap_bits.c
@@ -6,7 +6,7 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/14 15:44:03 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2026/04/14 16:08:17 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2026/04/14 16:13:43 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,19 @@ unsigned char	swap_bits(unsigned char octet)
 {
 	return ((octet >> 4) | (octet << 4));
 }
-
-
+/* 
 //test
 // #include <stdio.h>
 // int	main(void)
 // {
 // 	unsigned char x = 0b01000001; // x = 65 in decimal
 // 	unsigned char y = swap_bits(x);
-
 // 	printf("Before: %u\n", x);
 // 	printf("After:  %u\n", y);
-
 // 	return (0);
 // }
 // or to print it in binary format(bits)
 #include <unistd.h>
-
 void	print_bits(unsigned char octet)
 {
 	int	i;
@@ -49,13 +45,12 @@ int	main(void)
 {
 	unsigned char x = 65; // 01000001
 	unsigned char y;
-
 	print_bits(x);
 	write(1, "\n", 1);
-
 	y = swap_bits(x);
 	print_bits(y);
 	write(1, "\n", 1);
 
 	return (0);
 }
+ */
