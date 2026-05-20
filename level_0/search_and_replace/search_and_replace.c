4e89e70dd27a964d886d029c841d22e3d841fbd0
parvinghasemi <mghasemi.parvin@gmail.com>
2026-05-20 05:33:43 +0200

    adding search_and_replace and its readme

--- level_0/search_and_replace/search_and_replace.c
+++ level_0/search_and_replace/search_and_replace.c
@@ -10,3 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    char    old;
+    char    new;
+    char    *str;
+    int     i;
+    if (argc != 4 || argv[2][1] != '\0' || argv[3][1] != '\0')
+    {
+        write(1, "\n", 1);
+        exit(0);
+    }
+    old = argv[2][0];
+    new = argv[3][0];
+    str = argv[1];
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == old)
+            write(1, &new, 1);
+        else
+            write (1, &str[i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+    return (0);
+}
