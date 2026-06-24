0e748f05560edf490924eb02b63d32e2c405ba49
Parvin Mohammadghasemi <pamohamm@student.codam.nl>
2026-06-24 04:26:57 +0200

    adding expand_str and its README

---
+++ level_2/expand_str/expand_str.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_str.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pamohamm <pamohamm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 04:24:06 by pamohamm          #+#    #+#             */
+/*   Updated: 2026/06/24 04:26:29 by pamohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	is_space(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    int first_word;
+
+    if (argc == 2)
+    {
+        i = 0;
+        first_word = 1;
+        while (argv[1][i] != '\0')
+        {
+            while (is_space(argv[1][i]))
+                i++;
+            if (argv[1][i] != '\0')
+            {
+                if (!first_word)
+                    write(1, "   ", 3);
+                while (argv[1][i] != '\0'
+                    && !is_space(argv[1][i]))
+                {
+                    write(1, &argv[1][i], 1);
+                    i++;
+                }
+                first_word = 0;
+            }
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
+
