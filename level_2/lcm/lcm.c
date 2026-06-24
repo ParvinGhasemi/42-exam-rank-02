937316faaf8f007df9a7bf716b33d7741f2f74cc
Parvin Mohammadghasemi <pamohamm@student.codam.nl>
2026-06-24 07:43:26 +0200

    adding lcm and its README

---
+++ level_2/lcm/lcm.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lcm.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pamohamm <pamohamm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 07:27:56 by pamohamm          #+#    #+#             */
+/*   Updated: 2026/06/24 07:41:41 by pamohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int    lcm(unsigned int a, unsigned int b)
+{
+    unsigned int    lcm;
+
+    if (a == 0 || b == 0)
+        return (0);
+    if (a % b == 0)
+        return (a);
+    else if (b % a == 0)
+        return (b);
+    if (a > b)
+        lcm = a;
+    else
+        lcm = b;
+    while ((lcm % a != 0) || (lcm % b != 0))
+        lcm++;
+    return (lcm);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%u\n", lcm(12, 18)); // 36
+	printf("%u\n", lcm(4, 6));   // 12
+	printf("%u\n", lcm(5, 7));   // 35
+	printf("%u\n", lcm(0, 5));   // 0
+	return (0);
+}
+
